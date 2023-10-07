d0cb30a7dc1074a71af065ba382cdd2b24b3f4ae
Edmund_IdeaPad <elz.zhenming@gmail.com>
2023-10-07 21:00:04 +0800

    chore: fix indentation

--- sitApp/models.py
+++ sitApp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Customer(models.Model):
     user_id   = models.CharField(max_length=255, primary_key=True)
     user_name = models.CharField(max_length=255)
-
+    
     def __str__(self):
         return self.name
 
