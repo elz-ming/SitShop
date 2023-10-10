c086ad1235ff64888c4d36aad8fdf427b00952d8
ELZ_PC <elz.zhenming@gmail.com>
2023-10-11 00:11:25 +0800

    chore: set database to PostgreSQL to run vercel

--- sitProject/settings.py
+++ sitProject/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'sitProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE'  : 'django.db.backends.postgresql',
-#         'NAME'    : 'railway',
-#         'USER'    : 'postgres',
-#         'PASSWORD': '5DGEjSlYNIYzTqAqiSx9',
-#         'HOST'    : 'containers-us-west-63.railway.app',
-#         'PORT'    : '6666',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'  : 'django.db.backends.postgresql',
+        'NAME'    : 'railway',
+        'USER'    : 'postgres',
+        'PASSWORD': '5DGEjSlYNIYzTqAqiSx9',
+        'HOST'    : 'containers-us-west-63.railway.app',
+        'PORT'    : '6666',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
