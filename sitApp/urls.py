aaaf15712747a8d7700d825a7f808463c73dcbc1
ELZ_PC <elz.zhenming@gmail.com>
2023-10-14 20:55:51 +0800

    chore: delete redundant urls and views

--- sitApp/urls.py
+++ sitApp/urls.py
@@ -2,20 +2,28 @@ from django.urls import path, include
 from . import views
 from sitApp import views
 urlpatterns = [
-    path('', views.home, name='home'),                   # The URL and function to display home page
-    path('about/', views.about, name='about'),           # The URL and function to display about page
-    path('contact/', views.contact, name='contact'),     # The URL and function to display contacts page
-    # retrieve product based on the product_id variable
-    path('detail/<str:pid>/', views.detail, name='detail'), 
-    path('comparison/<str:product_list>', views.comparison, name='comparison'),
+    # The URL to display home page / product listing
+    path('', views.home, name='home'),                   
+    path('p/', views.home, name='allproduct'),
+    
+    # The URL to display user listing
+    path('u/', views.alluser, name='alluser'),
+
+    # The URL to display comparison of (up to) 4 products
+    path('comparison/<str:product_list>/', views.comparison, name='comparison'),
+
     # export pdf from comparison page
-    path('generate_pdf/<str:product_ids>', views.generate_pdf, name='generate_pdf'),
-    path('userdetail/<str:userid>', views.userdetail, name='userdetail'),
-    path('test/', views.test, name='test'),
-    path('challenge_response_test_view', views.challenge_response_test_view, name='challenge_response_test_view'),
-    path('verify_challenge_response', views.verify_challenge_response, name='verify_challenge_response'),
-    path('form_view', views.form_view, name='form_view'),
-    path('alluser/', views.alluser, name='alluser'),
+    path('generate_pdf/<str:product_ids>/', views.generate_pdf, name='generate_pdf'),
+
+    # retrieve product based on the product_id variable
+    path('p/<str:pid>', views.p_detail, name='p_detail'), 
+    
+    # retrieve product based on the product_id variable
+    path('u/<str:uid>', views.u_detail, name='u_detail'), 
+
+    # The URL and function to display about us page and contact us page
+    path('about/', views.about, name='about'),           
+    path('contact/', views.contact, name='contact')
 ]
 
 
--- sitApp/views.py
+++ sitApp/views.py
@@ -11,11 +11,9 @@ import requests
 import uuid
 import cgi
 
-challenge = uuid.uuid4().hex
 # Create your views here.
 
-# Alenna, work on this more.
-# Additionally, look at templates/components/searchbar.html & templates/pages
+
 def home(request):
     all_products  = Product.objects.all()
     search_query  = request.GET.get('q')
@@ -30,19 +28,28 @@ def home(request):
 
     return render(request, "pages/home.html", {'products':products, 'search_query':search_query})
 
-def about(request):
-    return render(request, "pages/about.html")
 
-def contact(request):
-    return render(request, "pages/contact.html")
+def alluser(request):
+    all_users  = User.objects.all()
+    search_query  = request.GET.get('q')
 
-# JingYu, work on this more.
-# Additionally, look at templates/components/comparison.html
+    if search_query:
+        all_users = all_users.filter(username__icontains=search_query)
+
+    paginator =  Paginator(all_users, 60)
+    
+    page = request.GET.get('page')
+    users = paginator.get_page(page)
+
+    return render(request, "pages/alluser.html", {'users':users, 'search_query':search_query})
+
+
 def comparison(request, product_list=None):
     products = product_list.split(',')
     products = [Product.objects.get(product_id=x) for x in products]
 
     return render(request, "pages/comparison.html", {'products':products, 'product_list':product_list})
+
 
 def generate_pdf(request, product_ids=None):
     products = product_ids.split(',')
@@ -78,154 +85,23 @@ def generate_pdf(request, product_ids=None):
     else:
         return HttpResponse('gg')
 
-def detail(request, pid):
+
+def p_detail(request, pid):
     product = get_object_or_404(Product, product_id=pid)
     return render(request, 'pages/detail.html', {'product': product})
 
-def render_to_pdf(request, context):
-    html_content = render_to_string('pages/comparison.html', context)
-    result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html_content.encode("UTF-8")), result)
-    if not pdf.err:
-        return result.getvalue()
-    return None
 
-# def generate_pdf(request, product_ids=None):
-#     # Split the product_ids string into a list of individual IDs
-#     product_ids = product_ids.split(',')
-
-#     # Retrieve products from the database based on the provided product_ids
-#     products = [Product.objects.get(product_id=product_id) for product_id in product_ids]
-
-#     # Convert queryset to a list of dictionaries
-#     products_data = [{'product_id': product.product_id,
-#                       'category': product.category,
-#                       'product_name': product.product_name,
-#                       'avg_rating': product.avg_rating,
-#                       'total_rating': product.total_rating,
-#                       'total_sold': product.total_sold,
-#                       'price': product.price,
-#                       'fav_count': product.fav_count,
-#                       'qty_avail': product.qty_avail,
-#                       'description': product.description,
-#                       'img_src': product.img_src} for product in products]
-
-#     context = {'products': products_data}
-
-#     # Generate PDF
-#     pdf = render_to_pdf(request, context)
-#     # if pdf:
-#     #     response = HttpResponse(pdf, content_type='application/pdf')
-#     #     filename = "comparison.pdf"
-#     #     content = "attachment; filename=%s" % filename
-#     #     response['Content-Disposition'] = content
-#     #     return response
-#     return HttpResponse("Hi")
-
-
-
-
-
-def search(request):
-    # defines what happens when there is a GET request
-
-    if request.method == "GET":
-        title = request.GET.get("q")
-        return render(request, 'pages/home.html')
-    # defines what happens when there is a POST request
-    else:
-        return render(request, 'pages/home.html')
-
-def cards_view(request):
-    # Your view logic goes here if needed
-    return render(request, 'pages/cards.html')
-
-
-
-
-
-# def comparison(request):
-#     return render(request, "pages/comparison.html", context={"cards": cards})
-
-def product_comparison_table(request):
-    products = Product.objects.all()
-    return render(request, 'pages/test.html', {'products': products})
-
-def test(request):
-    return render(request, "pages/test.html")
-
-def form_view(request):
-  # Validate the form
-  if contact.is_valid():
-    # Redirect the user to the challenge response test view
-    return HttpResponseRedirect('/challenge_response_test')
-
-  # Display the form again
-  return render(request, 'contact.html', {'contact':contact})
-
-
-def challenge_response_test_view(request):
-  # Generate a random challenge
-  challenge = uuid.uuid4().hex
-  # Save the challenge in the session
-  request.session['challenge'] = challenge
-  # Get the user's response
-  response = request.POST.get('response')
-   # If the response is correct, allow the user to submit the form
-  if response == challenge:
-    # Process the form submission
-    return HttpResponseRedirect('/success')
-   # Otherwise, display an error message
-  return render(request, 'error.html', message='Challenge-response test failed.')
-
-
-def verify_challenge_response(request):
-  # Get the challenge from the session
-  challenge = request.session.get('challenge')
-
-  # Get the user's response
-  response = request.POST.get('response')
-
-  # If the response is correct, allow the user to submit the form
-  if response == challenge:
-    return HttpResponseRedirect('/success')
-
-  # Otherwise, display an error message
-  return render(request, 'pages/error.html', message='Challenge-response test failed.')
-
-def contact_form_view(request):
-  # Validate the form
-  if contact.is_valid():
-    # Redirect the user to the challenge response test view
-    return HttpResponseRedirect('/challenge_response_test')
-
-  # Display the form again
-  return render(request, 'contact_form.html', {'contact': contact})
-
-def userdetail(request, userid):
-    user = get_object_or_404(User, username=userid)
+def u_detail(request, uid):
+    user = get_object_or_404(User, username=uid)
     return render(request, 'pages/userdetail.html', {'user': user})
 
-def alluser(request):
-    all_users  = User.objects.all()
-    search_query  = request.GET.get('q')
 
-    if search_query:
-        all_users = all_users.filter(username__icontains=search_query)
+def about(request):
+    return render(request, "pages/about.html")
 
-    paginator =  Paginator(all_users, 60)
-    
-    page = request.GET.get('page')
-    users = paginator.get_page(page)
 
-    return render(request, "pages/alluser.html", {'users':users, 'search_query':search_query})
+def contact(request):
+    return render(request, "pages/contact.html")
 
-def search1(request):
-    # defines what happens when there is a GET request
 
-    if request.method == "GET":
-        title = request.GET.get("q")
-        return render(request, 'pages/alluser.html')
-    # defines what happens when there is a POST request
-    else:
-        return render(request, 'pages/alluser.html')
+# End views.
